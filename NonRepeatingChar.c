4033b7d4f55df1dc90dda78b8768996d7cd675e9
Swopnalika Mohapatra <71244769+swopnalika@users.noreply.github.com>
2020-12-05 17:38:23 +0530

    Create NonRepeatingChar.c

---
+++ NonRepeatingChar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    //Initializing variables.
+    char str[100];
+    int i;
+    int freq[256] = {0};
+    
+    //Accepting inputs.
+    printf("Enter the string: ");
+    gets(str);
+    
+    //Calculating frequency of each character.
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+    
+    
+    printf("The non repeating characters are: ");
+    for(i = 0; i < 256; i++)
+    {
+        if(freq[i] == 1)//Finding uniques charcters and printing them.
+        {
+            printf(" %c ", i);
+        }
+    }
+    return 0;
+}
